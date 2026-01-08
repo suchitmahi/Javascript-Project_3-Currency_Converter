0dd9f22a3fae8968475d2462b36c285d662c9b92
Suchit Mahi <suchitmahi14@gmail.com>
2026-01-09 00:24:25 +0530

    enter amount will be reset if its less than 1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1"
 
 const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
 
 for(let select of dropdowns){
     for (currCode in countryList) {
@@ -26,3 +27,13 @@ const updateFlag = (element) => {
     let img = element.parentElement.querySelector("img");
     img.src = newSrc;
 }
+
+btn.addEventListener("click", (evt) => {
+    evt.preventDefault();
+    let amount = document.querySelector(".amount input");
+    let amtVal = amount.value;
+    if (amtVal === "" || amtVal < 1){
+        amtVal = 1;
+        amount.value = "1";
+    }
+});
