2c6d2ed3a548f54a3fdda8f4c5c8f9fc30a1a4d9
Suchit Mahi <suchitmahi14@gmail.com>
2026-01-08 23:19:20 +0530

    added selected for both us and india

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ for(let select of dropdowns){
         let newOption = document.createElement("option");
         newOption.innerText = currCode;
         newOption.value = currCode;
+        if (select.name === "from" && currCode === "USD"){
+            newOption.selected = "selected";
+        } else if (select.name === "to" && currCode === "INR") {
+            newOption.selected = "selected";
+        }
         select.append(newOption);
     }
 }
